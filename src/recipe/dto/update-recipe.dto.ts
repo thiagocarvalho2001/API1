aa819f982d7d2ef08be5cf21834de01b90bfb310
Thiago de Carvalho <thiagovowclash@gmail.com>
2024-02-07 23:43:28 -0300

    first

---
+++ src/recipe/dto/update-recipe.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import { PartialType } from '@nestjs/swagger';
+import { CreateRecipeDto } from './create-recipe.dto';
+
+export class UpdateRecipeDto extends PartialType(CreateRecipeDto) {}
